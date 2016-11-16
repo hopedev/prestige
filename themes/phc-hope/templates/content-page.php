9339ad275146a66454f0cf2391fea8005f9d2efb
hopedev <elvina@hope.agency>
2016-11-16 17:50:52 +0000

    more quote styling

--- themes/phc-hope/templates/content-page.php
+++ themes/phc-hope/templates/content-page.php
@@ -1,29 +1,37 @@
 
-<div class="uk-grid uk-grid-width-medium-1-2">
+<div class="page-content uk-grid uk-grid-width-medium-1-2 uk-grid-large">
 	<div>
 		<div class="featured-image">
 		<?php the_post_thumbnail("medium"); ?>
 		 </div>
-		<div class="featured-quote"><blockquote>
+		
 		<?php 
 			$quote =  get_post_meta( get_the_ID(), 'featured_quote', true );
-			if( $quote ) { echo $quote;  }
-			?>
-			</blockquote>
-			<div class="quote-author">
-					<?php 
-			$quote_author =  get_post_meta( get_the_ID(), 'featured_quote_author', true );
-			if( $quote_author ) { echo $quote_author;  }
-			?>
-			</div>
+			if( $quote ) { 
+				echo '<div class="featured-quote"><blockquote>';
+				echo $quote;  
+				echo '</blockquote>';
+				$quote_author =  get_post_meta( get_the_ID(), 'featured_quote_author', true );
+				if( $quote_author ) { 
+					echo '<div class="quote-author">';
+					echo $quote_author;  
+					echo '</div>';
+				}
+				
+			}
+		?>
+
+			
+
+			
 		 </div>
 
 
 	 </div>
 
-<div>
-	<?php the_content(); ?>
-</div>
+	<div class="post-content">
+		<?php the_content(); ?>
+	</div>
 </div>
 
 
